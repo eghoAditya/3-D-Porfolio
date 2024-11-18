1cafa2aaba3dd2d75094f2495c499f7af337f3d0
Aditya <jackuserme8881@gmail.com>
2024-11-18 17:23:42 +0530

    assets

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import {HTML} from '@react-three/drei'
+import React from 'react';
+import { Html } from '@react-three/drei'; // Use the correct import for Html
 
 const Loader = () => {
     return (
         <Html>
             <div className='flex justify-center items-center'>
-                <div className=' w-20 h-20 border-2 
+                <div className='w-20 h-20 border-2 
                  border-opacity-20 border-blue-500
                  border-t-blue-500 rounded-full animate-spin' />
             </div>
         </Html>
-      
-  )
+    );
 }
 
-export default Loader
+export default Loader;
